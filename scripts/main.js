e22abafd563e6d6e05570f7114632f0e6b55b7d0
Aleksey Blaginykh <alexey0975@yandex.ru>
2022-09-07 16:59:42 +0500

    starting created burger

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,85 @@
 document.addEventListener('DOMContentLoaded', () => {
+  class Burger {
+    constructor(selector, params) {
+      this.btn = document.querySelector(selector);
+      [this.parrent] = document.getElementsByClassName(params.parrentClass || 'js-burger-parrent');
+      this.elements = params.elementsClass || 'js-menu';
+
+      if (!this.parrent) throw new Error('Родительский элемент бургера не найден');
+      if (!this.btn) throw new Error('Кнопка бургера не найдена');
+
+      this.parrent.style.position = 'relative';
+
+      if (params.breakpoints && Object.keys(params.breakpoints).length) {
+        this.breakpoint = params.breakpoints;
+        this.elements = this.breakpoint === Infinity ?
+          params.elementsClass : params.breakpoints[this.breakpoint].elementsClass;
+
+        let currentBreakpoint = this.breakpoint;
+
+        window.addEventListener('resize', () => {
+          this.breakpoint = params.breakpoints;
+
+          if (this.breakpoint !== currentBreakpoint) {
+            const classPath = this.breakpoint !== Infinity ?
+              params.breakpoints[this.breakpoint].elementsClass :
+              params.elementsClass;
+
+            this.elements.forEach(element => element.style.display = null);
+            this.elements = classPath;
+
+            currentBreakpoint = this.breakpoint;
+          }
+        });
+      }
+
+      this.btn.addEventListener('click', () => {
+        this.toggleMenu();
+      })
+    }
+
+    get windowWith() {
+      return window.innerWidth;
+    }
+
+    get elements() {
+      return this._elements;
+    }
+
+    set elements(value) {
+      this._elements = Array.from(document.getElementsByClassName(value));
+      this._elements.forEach(element => element.style.display = 'none');
+    }
+
+    get breakpoint() {
+      return this._breakpoint;
+    }
+
+    set breakpoint(breakpoints) {
+      let currentBreakpoint = Infinity;
+
+      Object.keys(breakpoints).forEach(key => {
+        const breakpoint = Number(key);
+        if (
+          currentBreakpoint > breakpoint &&
+          breakpoint >= this.windowWith
+        ) {
+          currentBreakpoint = breakpoint;
+        }
+      });
+
+      this._breakpoint = currentBreakpoint;
+    }
+
+    toggleMenu() {
+      this.elements.forEach(element => {
+        if (element.style.display === 'none')
+          element.style.display = null;
+        else element.style.display = 'none';
+      });
+    }
+  }
+
   // Custom select
   new Choices('.js-select', {
     searchEnabled: false,
@@ -44,6 +125,18 @@ document.addEventListener('DOMContentLoaded', () => {
       321: {
         slidesPerView: 2,
         spaceBetween: 30,
+      },
+    },
+  });
+
+  new Burger('.js-burger', {
+    breakpoints: {
+      768: {
+        elementsClass: 'js-menu-768',
+      },
+
+      320: {
+        elementsClass: 'js-menu-320',
       },
     },
   });
