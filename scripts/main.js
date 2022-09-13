291ad3a41855ee4de9a2db2dbc80c2740940c404
Aleksey Blaginykh <alexey0975@yandex.ru>
2022-09-13 16:59:33 +0500

    burger done, modal added

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
     #breakpoints
     #_elements
     #_breakpoint
+    #_items
 
-    constructor(selector, params) {
+    constructor(selector, params = {}) {
       this.#breakpoints = params.breakpoints || null;
       this.btn = document.querySelector(selector);
       this.parrentClass = params.parrentClass || 'js-burger-parrent';
+      this.itemsClass = params.itemsClass || 'js-burger__item'
       this.elements = params.elementsClass || 'js-menu';
+      this.items = params.itemsClass || 'js-burger__item';
+
+      this.menu = document.createElement('div');
+      this.menu.classList.add(...['burger__menu', 'burger__menu--closed']);
+      this.menu.style.position = 'absolute';
+      this.menu.style.left = 0;
+      this.menu.style.width = '100vw';
+      this.menu.style.height = '100vh';
+      this.menu.style.zIndex = '99';
+      this.menu.style.transform = 'translateX(-110vw)';
+      this.menu.style.overflow = 'hidden';
 
       this.btn.style.position = 'relative';
       this.btn.style.zIndex = '199';
@@ -18,25 +31,26 @@ document.addEventListener('DOMContentLoaded', () => {
       if (!this.btn) throw new Error('Кнопка бургера не найдена');
 
       this.parrent.style.position = 'relative';
+      this.parrent.prepend(this.menu);
 
       if (params.breakpoints && Object.keys(params.breakpoints).length) {
         this.breakpoint = params.breakpoints;
         this.elements = this.elementClassPath;
-
+        this.items = this.itemsClassPath;
         let currentBreakpoint = this.breakpoint;
 
         window.addEventListener('resize', () => {
           this.breakpoint = params.breakpoints;
 
           if (this.breakpoint !== currentBreakpoint) {
+            this.closeMenu();
             this.elements.forEach(element => {
-              element.classList.remove('menu--closed');
-              element.style.position = null;
-              element.style.transition = null;
-              element.style.transform = null;
+              element.style.display = null;
             });
-            this.elements = this.elementClassPath;
 
+            this.menu.textContent = '';
+            this.elements = this.elementClassPath;
+            this.items = this.itemsClassPath;  
             currentBreakpoint = this.breakpoint;
           }
         });
@@ -47,15 +61,34 @@ document.addEventListener('DOMContentLoaded', () => {
       })
     }
 
+    get items() {
+      return this.#_items;
+    }
+
+    set items(itemClass) {
+      this.#_items = document.getElementsByClassName(itemClass);
+      Array.from(this.#_items).forEach(item => {
+        item.addEventListener('click', () => {
+          this.closeMenu();
+        });
+      });
+    }
+
     get parrent() {
       const [parrent] = document.getElementsByClassName(this.parrentClass);
       return parrent;
     }
 
     get elementClassPath () {
-      return this.breakpoint ?
+      return this.breakpoint !== Infinity ?
         this.#breakpoints[this.breakpoint].elementsClass :
         this.elementsClass || 'js-menu';
+    }
+
+    get itemsClassPath () {
+      return this.breakpoint !== Infinity ?
+        this.#breakpoints[this.breakpoint].itemsClass :
+        this.itemsClass || 'js-burger__item';
     }
 
     get windowWith() {
@@ -68,10 +101,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     set elements(value) {
       this.#_elements = Array.from(document.getElementsByClassName(value));
+
       this.#_elements.forEach(element => {
-        element.style.position = 'absolute';
-        element.style.transform = 'translateX(-110vw)';
-        element.classList.add('menu--closed');
+        const cloneEl = element.cloneNode(true);
+        element.style.display = 'none';
+        this.menu.append(cloneEl);
       });
     }
 
@@ -80,36 +114,107 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     set breakpoint(breakpoints) {
-      this.#_breakpoint = 0;
-
+      this.#_breakpoint = Infinity;
       Object.keys(breakpoints).forEach(key => {
         const breakpoint = Number(key);
         if (
-          this.#_breakpoint <= breakpoint &&
-          breakpoint > this.windowWith
+          breakpoint >= this.windowWith &&
+          breakpoint <= this.#_breakpoint
         ) {
           this.#_breakpoint = breakpoint;
         }
       });
     }
 
+    openMenu() {
+      this.menu.style.transition = 'transform 600ms ease';
+      this.menu.style.transform = null;
+      this.menu.classList.remove('burger__menu--closed');
+      this.btn.classList.add('burger--close');
+      document.body.style.overflow = 'hidden';
+    }
+
+    closeMenu() {
+      this.menu.style.transform = 'translateX(-110vw)';
+      this.menu.style.transition = 'transform 600ms ease';
+      this.menu.classList.add('burger__menu--closed');
+      this.btn.classList.remove('burger--close');
+      document.body.style.overflow = null;
+    }
+
     toggleMenu() {
-      this.elements.forEach(element => {
-        if (element.classList.contains('menu--closed')) {
-          element.style.transition = 'transform 600ms ease';
-          element.style.transform = null;
-          element.classList.remove('menu--closed');
-          this.btn.classList.add('burger--close');
-          document.body.style.overflow = 'hidden';
-        }
-        else {
-          element.style.transform = 'translateX(-110vw)';
-          element.style.transition = 'transform 600ms ease';
-          element.classList.add('menu--closed');
-          this.btn.classList.remove('burger--close');
-          document.body.style.overflow = null;
-        };
+      if (this.menu.classList.contains('burger__menu--closed')) this.openMenu();
+      else this.closeMenu();
+    }
+  }
+
+  class Modal {
+
+    constructor(selector, params = {}) {
+      this.modal = document.querySelector(selector || '.js-modal');
+      this.openBtnClass = params.openBtnClass || 'js-modal__open';
+      this.doneBtnClass = params.doneBtnClass ||'js-modal__done';
+      this.wrapperClass = params.wrapperClass || 'js-modal-wrapper';
+      this.closeBtnClass = params.closeBtnClass || 'js-modal__close';
+
+      [this.modalCloseBtn] = document.getElementsByClassName(this.closeBtnClass);
+      [this.modalOpenBtn] = document.getElementsByClassName(this.openBtnClass);
+      [this.modalDoneBtn] = document.getElementsByClassName(this.doneBtnClass);
+
+      this.modalWrapper = document.createElement('div');
+      this.modalWrapper.classList.add(this.wrapperClass);
+      this.modalWrapper.style.position = 'absolute';
+      this.modalWrapper.style.width = '100vw';
+      this.modalWrapper.style.height = '100vh';
+      this.modalWrapper.style.left = 0;
+      this.modalWrapper.style.top = 0;
+      this.modalWrapper.style.backgroundColor = 'rgba(0, 0, 0, .7)';
+      this.modalWrapper.style.zIndex = '299';
+      this.modalWrapper.classList.add('js-modal--close');
+      this.modalWrapper.style.display = 'none';
+      document.body.append(this.modalWrapper);
+      this.modalWrapper._isWrapper = true;
+
+      if (this.modal) {
+        this.modalWrapper.append(this.modal);
+        this.modal.classList.add('js-modal--capture');
+      }
+
+      this.modalWrapper.addEventListener('click', (event) => {
+        if(!event.target._isWrapper) return;
+        this.closeModal();
       });
+
+      this.modalOpenBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        this.openModal();
+      });
+
+      this.modalCloseBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        this.closeModal();
+      });
+
+      this.modalDoneBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (params.doneAction) {
+          doneAction();
+        } else {
+          this.closeModal();
+        }
+      })
+    }
+
+    openModal() {
+      this.modalWrapper.style.display = 'block';
+      document.body.style.overflow = 'hidden';
+      this.modalWrapper.classList.remove('js-modal--close');
+    }
+
+    closeModal() {
+      this.modalWrapper.style.display = 'none';
+      document.body.style.overflow = null;
+      this.modalWrapper.classList.add('js-modal--close');
     }
   }
 
@@ -162,15 +267,19 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   });
 
-  const burger = new Burger('.js-burger', {
+  new Burger('.js-burger', {
     breakpoints: {
       768: {
         elementsClass: 'js-menu-768',
+        itemsClass: 'js-burger__link-768',
       },
 
       320: {
         elementsClass: 'js-menu-320',
+        itemsClass: 'js-burger__link-320',
       },
     },
   });
+
+  new Modal('.js-modal');
 })
